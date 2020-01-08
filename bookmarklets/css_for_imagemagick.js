4d54c1bb0bf69ae25d8ca256ac7bdac1c7993246
John Muccigrosso <john@muccigrosso.org>
2020-01-08 11:13:12 +0100

    Initial commit

---
+++ bookmarklets/css_for_imagemagick.js
@@ -0,0 +1,2 @@
+#Makes ImageMagick tutorial pages more readable.
+javascript:function%20apply_css()%7Bvar%20element=document.createElement('style'),sheet;document.head.appendChild(element);sheet=element.sheet;sheet.insertRule('body%20{background-color:white;margin-right:10%25;margin-left:10%25;font-family:"Gill Sans"}',0);sheet.insertRule('tr%20{margin:1em;}',0);%7D%20apply_css();
